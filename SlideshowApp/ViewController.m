d3867cfb3be36d2d50aac53e25a2a24f4fff6edc
tgaiacontentsdev <tgaiacontentsdev@tgaiacontentsdev-no-MacBook-Pro.local>
2016-01-05 11:59:12 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SlideshowApp
+//
+//  Created by tgaiacontentsdev on 2016/01/05.
+//  Copyright (c) 2016年 tgaiacontentsdev. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
